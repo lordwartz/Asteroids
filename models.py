0fe636f79aa93cbadd487eeb035173a825808b28
dudeFromTheInternet <ilyaratush@gmail.com>
2023-05-18 14:44:34 +0500

    Added game logic for Asteroids and Spaceship

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
+from utils import get_random_velocity, load_sprite, wrap_position
 
 
 class GameObject:
@@ -12,9 +14,41 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
+
+
+class Spaceship(GameObject):
+    MANEUVERABILITY = 3
+    ACCELERATION = 0.25
+
+    def __init__(self, position):
+        self.direction = Vector2(0, -1)
+        super().__init__(position, load_sprite("spaceship"), Vector2(0))
+
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(Vector2(0, -1))
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
+
+
+class Asteroid(GameObject):
+    def __init__(self, position):
+        super().__init__(
+            position, load_sprite("asteroid"), get_random_velocity(1, 3)
+        )
+
